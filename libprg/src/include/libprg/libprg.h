db8af014ad3a4aea755a62dd6c6d6790d777e3b0
rebecac24 <rebeca.c24@aluno.ifsc.edu.br>
2025-09-04 21:09:47 -0300

    salvando alterações

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,7 +1,16 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
-#define TAM_MAX 100
+#include <stdbool.h>
 
+typedef struct pilha pilha_t;
 
+// Comportamentos da pilha
+pilha_t* criar_pilha(int capacidade);
+void empilhar(pilha_t* pilha, int elemento);
+int desempilhar(pilha_t* pilha);
+int tamanho_pilha(pilha_t* pilha);
+bool pilha_vazia(pilha_t* pilha);
+int topo_pilha(pilha_t* pilha);
+void destruir_pilha(pilha_t* pilha);
 
 #endif
--- libprg/src/libprg/pilha.c
+++ libprg/src/libprg/pilha.c
@@ -4,6 +4,7 @@
 #include "libprg/libprg.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct pilha {
     int* elementos;
@@ -24,7 +25,40 @@ pilha_t* criar_pilha(int capacidade){
     return p;
 }
 
-void destruir_pilha(pilha_t** pp) {
-
+void empilhar(pilha_t* pilha,int valor) {
+    if (pilha->topo < pilha->capacidade -1) {
+        pilha->topo++;
+        pilha->elementos[pilha->topo] = valor;
+    }
 }
+
+
+int desempilhar(pilha_t* pilha) {
+    if (pilha->topo < 0) {
+        return -1; // Pilha vazia
+    }
+    int elemento = pilha->elementos[pilha->topo];
+    pilha->topo--;
+    return elemento;
+}
+
+int tamanho_pilha(pilha_t* pilha) {
+    return pilha->topo + 1;
+}
+
+bool pilha_vazia(pilha_t* pilha) {
+    return pilha->topo < 0;
+}
+
+int topo_pilha(pilha_t* pilha) {
+    if (pilha->topo < 0) {
+        return -1; // Pilha vazia
+    }
+    return pilha->elementos[pilha->topo];
+}
+
+void destruir_pilha(pilha_t* pilha) {
+        free(pilha->elementos);
+        free(pilha);
+    }
 
